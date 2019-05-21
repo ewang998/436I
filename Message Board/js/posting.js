50d0aa40ff074a4ac81cd96639b3db4fd7ad3e0f
ewang998 <ewang998@alumni.ubc.ca>
2019-05-21 14:43:18 -0700

    version 1.1 - added delete deature

--- Message Board/js/posting.js
+++ Message Board/js/posting.js
@@ -9,8 +9,20 @@ document.addEventListener("DOMContentLoaded", function() {
   loadboard();
 });
 
+//delete a specific message
+function deleteli() {
+  var num = document.getElementById("linumber").value;
+  var elem = document.getElementById(num);
+  try {
+    elem.parentNode.removeChild(elem);
+  } catch(err) {
+    alert("id does not exist!");
+  }
+}
+
 //display the messages hold in initial string
 function loadboard() {
+  clearall();
   //create object from string
   var myJSON = JSON.parse(originmessage);
   //access ul from html
@@ -18,6 +30,8 @@ function loadboard() {
   //loop thought message and add each as li
   for (var curr in myJSON) {
     var node = document.createElement("LI");
+    //CS436I - click function to show id
+    node.addEventListener("click",Alert);
     node.setAttribute("id", counter);
     var textnode = document.createTextNode(myJSON[curr]);
     node.appendChild(textnode);
@@ -40,6 +54,8 @@ function addboard() {
   var adding = document.getElementById("newtext").value;
   var node = document.createElement("LI");
   var textnode = document.createTextNode(adding);
+  //CS436I - click function to show id
+  node.addEventListener("click",Alert);
   node.appendChild(textnode);
   node.setAttribute("id", counter);
   //checkbox value is checked here
@@ -56,7 +72,12 @@ function addboard() {
   counter++;
 }
 
+//message when clicked on message
+function Alert(){
+  console.log(this.id);
+  alert("you have clicked the message with id: " + this.id);
+  }
+
 function cleartext() {
   document.getElementById("newtext").value = "";
-  console.log("sth");
 }
